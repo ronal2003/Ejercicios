b30717b23d07d7cf4098634bb1505fecbdb8d441
ronal2003 <ronuldox.2003@gmail.com>
2022-07-07 15:48:39 -0500

    primer subida de los ejercicios

---
+++ src/constantes.java
@@ -0,0 +1,11 @@
+
+public class constantes {
+
+	public static void main(String[] args) {
+		final int cons = 12; // las constanes de declaran con una palbara inicial esta es final
+		
+		//cons = 10;
+		System.out.println("constante: " + cons);
+	}
+
+}
